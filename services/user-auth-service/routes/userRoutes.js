e1f04f0e111fc86a42b5052f2628e5a89c4a03d2
YasserS <56772311+YasserS-21@users.noreply.github.com>
2024-12-02 15:55:43 -0500

    fixed missing import

--- services/user-auth-service/routes/userRoutes.js
+++ services/user-auth-service/routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const authMiddleware = require("../middleware/authMiddleware")
 
-const { registerUser, loginUser } = require('../controllers/userController');
+const { registerUser, loginUser, getProfile } = require('../controllers/userController');
 
 const router = express.Router();
 
